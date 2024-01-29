3c73b41b53ca3ce49d694ced8770bd70ea89620d
David Ciucă <ciucadavidstefan@gmail.com>
2024-01-29 22:58:33 +0200

    deleteOption

--- Marketplace/item/views.py
+++ Marketplace/item/views.py
@@ -8,7 +8,6 @@ def details(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category = item.category, is_sold = False).exclude(pk=pk)[0:3]
 
-
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
@@ -32,3 +31,10 @@ def new(request):
         'form': form,
         'title': 'New Item',
     })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk = pk, created_by = request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
